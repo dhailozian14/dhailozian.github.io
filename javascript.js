8e8a0b0e1f2d513b61c7c88202b77def376a115d
dhailozian <dhailozian14@smpanthers.org>
2014-05-12 14:10:46 -0700

    JS

---
+++ javascript.js
@@ -0,0 +1,8 @@
+                        function slideDown() {
+                                $("#foo").html("I'm also going to UCSC and have more than 15 relatives in the Philippines.");
+                                $("#foo").slideDown();
+                  }
+                        function slideUp() {
+                                $("#foo").html("Hiding Information");
+                                $("#foo").slideUp();
+                  }
